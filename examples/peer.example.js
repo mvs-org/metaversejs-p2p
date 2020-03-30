01a01fc5b6df1c86eba690247e9bd3515c531ce9
canguruhh <sven@mutzl.org>
2020-03-30 22:28:41 +0200

    Example for peer

--- examples/peer.example.js
+++ examples/peer.example.js
@@ -1,4 +1,4 @@
-const { Peer, PingMessage } = require('../lib')
+const { Peer, PingMessage, GetBlocksMessage, MemPoolMessage, GetAddrMessage } = require('../lib')
 const { P2PTransportTCPIP } = require('metaversejs-transport-tcpip')
 
 const transport = new P2PTransportTCPIP({host: '198.199.84.199', port: 5251})
@@ -10,15 +10,23 @@ const peer = new Peer({
 }).connect()
 
 peer.$egress.subscribe(message => {
-    console.log('-->> egress to',peer.host, message.command, message.payload)
+    console.log('-->> egress to',peer.host, message.command, message.payload ? message.payload : '')
 })
 
 peer.$ingress.subscribe(message => {
-    console.log('<<-- ingress from',peer.host, message.command, message.payload)
+    console.log(JSON.stringify(message.payload, null, 4))
+    console.log('<<-- ingress from',peer.host, message.command, message.payload ? message.payload : '')
 })
 
-setInterval(() => { peer.sendMessage(new PingMessage()) }, 5000)
+// setInterval(() => { peer.sendMessage(new PingMessage()) }, 5000)
+setTimeout(() => { peer.sendMessage(new GetBlocksMessage({
+    startHashes: ['e5ce278c601f5e326de3210f2871ede99e4f8db740dc93c45205232765d1d3a5'],
+})) }, 2000)
 
 peer.$status.subscribe(status=>{
+    if(status===Peer.STATUS.READY){
+        peer.sendMessage(new MemPoolMessage())
+        peer.sendMessage(new GetAddrMessage())
+    }
     console.log('connection status:', status)
 })
