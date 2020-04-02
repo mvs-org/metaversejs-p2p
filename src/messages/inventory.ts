9fcded9946a1a3a2e57f23bfa9ad2aee761555b4
canguruhh <sven@mutzl.org>
2020-04-02 12:09:37 +0200

    change inventory payload

--- src/messages/inventory.ts
+++ src/messages/inventory.ts
@@ -2,47 +2,45 @@ import { Message } from './message'
 import { readSlice, toUInt32LE, readUInt32LE, BufferState, readVarInt, toVarInt } from '../encoding'
 
 export class InventoryItem{
-    constructor(public type: number, public hash: string){}
-}
-export class InventoryTx extends InventoryItem {
-    constructor(public hash: string) {
-        super(InventoryMessage.TYPES.TX, hash)
-    }
-}
-export class InventoryBlock extends InventoryItem{
-    constructor(public hash: string) {
-        super(InventoryMessage.TYPES.BLOCK, hash)
-    }
-}
-
-export interface InventoryMessagePayload {
-    inventory: InventoryItem[]
-}
-
-export class InventoryMessage extends Message {
     static TYPES = {
         TX: 1,
         BLOCK: 2,
     }
+    constructor(public type: number, public hash: string){}
+}
+
+export class InventoryTx extends InventoryItem {
+    constructor(public hash: string) {
+        super(InventoryItem.TYPES.TX, hash)
+    }
+}
+
+export class InventoryBlock extends InventoryItem{
+    constructor(public hash: string) {
+        super(InventoryItem.TYPES.BLOCK, hash)
+    }
+}
+
+export class InventoryMessage extends Message {
     getPayload() {
         return Buffer.concat([
-            toVarInt(this.payload.inventory.length),
+            toVarInt(this.payload.length),
             Buffer.concat(
-            this.payload.inventory.map(
+            this.payload.map(
                 item => Buffer.concat([
                     toUInt32LE(item.type),
                     Buffer.from(item.hash, 'hex').reverse(),
                 ], 36),
-            ), this.payload.inventory.length * 36,
+            ), this.payload.length * 36,
         )])
     }
     static parseInventoryItem(bufferState: BufferState){
         const type = readUInt32LE(bufferState)
         const hash = readSlice(bufferState, 32).reverse().toString('hex')
         switch(type){
-            case InventoryMessage.TYPES.TX:
+            case InventoryItem.TYPES.TX:
                 return new InventoryTx(hash)
-            case InventoryMessage.TYPES.BLOCK:
+            case InventoryItem.TYPES.BLOCK:
                 return new InventoryBlock(hash)
             default:
                 return new InventoryItem(type, hash)
@@ -55,12 +53,9 @@ export class InventoryMessage extends Message {
         for(let i=0; i<count; i++){
             inventory.push(InventoryMessage.parseInventoryItem(bufferState))
         }
-        
-        return new InventoryMessage({
-            inventory,
-        })
+        return new InventoryMessage(inventory)
     }
-    constructor(public payload: InventoryMessagePayload) {
+    constructor(public payload: InventoryItem[]) {
         super('inv')
     }
 }
